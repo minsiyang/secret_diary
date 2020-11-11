b24a596f7c6172ba3c7312c42309982568254ec5
Minsi Yang <yangminsi@gmail.com>
2020-11-11 15:49:01 +0000

    Added #unlock, test passed

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,13 +1,19 @@
 class SecretDiary
   def initialize
     @entries = []
+    @locked = true
   end
 
   def add_entry(diary)
+    return "You need to unlock!" if @locked
     @entries << diary
   end
 
   def get_entries
     @entries
+  end
+
+  def unlock
+    @locked = false
   end
 end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -1,24 +1,40 @@
 require "secret_diary"
 
 RSpec.describe SecretDiary do
-  context "user can add an entry" do
+
+  context "user has to unlock the diary in order to use" do
     it "adds the diary entry to an array" do
       secret_diary = SecretDiary.new
       diary = "This is my first piece"
       entries = [diary]
+      secret_diary.unlock
       expect(secret_diary.add_entry(diary)).to eq(entries)
     end
-  end
 
-  context "user can retrieve entries" do
     it "get added entries" do
       secret_diary = SecretDiary.new
       diary1 = "This is my first piece"
       diary2 = "This is my second piece"
+      secret_diary.unlock
       secret_diary.add_entry(diary1)
       secret_diary.add_entry(diary2)
       entries = [diary1, diary2]
       expect(secret_diary.get_entries).to eq(entries)
+    end
+  end
+
+  context "user could not acces to the diary if it is locked" do
+    it "could not add an diary entry" do
+      secret_diary = SecretDiary.new
+      diary = "This is my first piece"
+      expect(secret_diary.add_entry(diary)).to eq("You need to unlock!")
+    end
+
+    it "could not get added entries" do
+      secret_diary = SecretDiary.new
+      diary1 = "This is my first piece"
+      diary2 = "This is my second piece"
+      expect(secret_diary.get_entries).to eq("You need to unlock!")
     end
   end
 end
