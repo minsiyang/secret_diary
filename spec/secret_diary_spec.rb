0cb88fc10426e5cbe09e48aad47d3d83ea94f25e
Minsi Yang <yangminsi@gmail.com>
2020-11-16 11:59:24 +0000

    Refactored tests for Secret Diary Class

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -1,27 +1,24 @@
 require "secret_diary"
 
 RSpec.describe SecretDiary do
+  let(:diary) { instance_double("Diary") }
+  let(:secret_diary) { SecretDiary.new(diary) }
 
   context "user has to unlock the diary in order to use" do
-    it "adds the diary entry to an array" do
-      diary = instance_double("Diary")
-      secret_diary = SecretDiary.new(diary)
+    before(:each) do
       secret_diary.unlock
       expect(diary).to receive(:add).with("First entry", "This is my first piece")
       secret_diary.add_entry("First entry", "This is my first piece")
-      
+    end
+
+    it "adds the diary entry to an array" do
       secret_diary.lock
       expect(diary).not_to receive(:add).with("Second entry", "This is my second piece")
       expect{ secret_diary.add_entry("Second entry", "This is my second piece") }.to raise_error("You need to unlock!")
     end
 
     it "get added entries" do
-      diary = instance_double("Diary")
-      secret_diary = SecretDiary.new(diary)
-      secret_diary.unlock
-      allow(diary).to receive(:add).with("First entry", "This is my first piece")
-      secret_diary.add_entry("First entry", "This is my first piece")
-      allow(diary).to receive(:add).with("Second entry", "This is my second piece")
+      expect(diary).to receive(:add).with("Second entry", "This is my second piece")
       secret_diary.add_entry("Second entry", "This is my second piece")
 
       expect(diary).to receive(:view)
@@ -33,17 +30,15 @@ RSpec.describe SecretDiary do
   end
 
   context "user could not acces to the diary if it is locked" do
-    it "could not add an diary entry" do
-      diary = instance_double("Diary")
-      secret_diary = SecretDiary.new(diary)
+    before(:each) do
       expect(diary).not_to receive(:add).with("First entry", "This is my first piece")
+    end
+
+    it "could not add an diary entry" do
       expect{ secret_diary.add_entry("First entry", "This is my first piece") }.to raise_error("You need to unlock!")
     end
 
     it "could not get added entries" do
-      diary = instance_double("Diary")
-      secret_diary = SecretDiary.new(diary)
-      expect(diary).not_to receive(:add).with("First entry", "This is my first piece")
       expect(diary).not_to receive(:view)
       expect{ secret_diary.get_entries }.to raise_error("You need to unlock!")
     end
